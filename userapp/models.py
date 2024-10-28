22e30ccc089594360bc6f3c228bd8a55fe9419af
Ali Ahmadi <aliahmadi79sh@gmail.com>
2024-10-28 16:43:27 +0330

    Start Project - Create User App - Create User Model

---
+++ userapp/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None):
+        """
+        Creates and saves a User with the given email, and password.
+        """
+
+        user = self.model(
+            phone=phone,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            phone=phone,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    phone = models.CharField(verbose_name="تلفن همراه", max_length=11, unique=True)
+    email = models.EmailField(verbose_name="آدرس ایمیل", max_length=255)
+    fullname = models.CharField(max_length=250, verbose_name="نام کامل")
+    is_active = models.BooleanField(default=True, verbose_name="وضعیت")
+    is_admin = models.BooleanField(default=False, verbose_name="ادمین")
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.phone}-{self.email}'
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+class Otp(models.Model):
+    token = models.CharField(verbose_name="توکن", max_length=200, null=True)
+    phone = models.CharField(verbose_name="تلفن همراه", max_length=11, unique=True)
+    code = models.SmallIntegerField(verbose_name="کد تائید")
+    expiration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.phone}->{self.code}'
